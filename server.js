5dee791669d5015b00a81c841bb16906a92793eb
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2022-11-04 00:24:34 -0700

    with dias integrated

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
-require("http").createServer((inRequest, inResponse) => {
-        inResponse.end("Hello from first server");
-    }
-).listen(80)
+const http = require("http")
+const request = require("request")
+const fs = require("fs").promises
+
+
+handler = (inRequest, inResponse) => {
+    fs.readFile(__dirname + "/assets/html/cart.html").then(content => {
+            inResponse.setHeader("Content-Type", "text/html");
+            inResponse.writeHead(200);
+            inResponse.end(content);
+        }
+    ).catch( err => {
+        inResponse.writeHead(500);
+        inResponse.end(err)
+        }
+    )
+}
+http.createServer(handler).listen(80)
+
+
