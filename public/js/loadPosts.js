41f59a6d1ec0845d7656df56fb46f67a69e90763
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2022-11-05 00:54:09 -0700

    button features

--- public/js/loadPosts.js
+++ public/js/loadPosts.js
@@ -11,12 +11,18 @@ class aPost extends React.Component {
 }
 
 function start() {
+    nextPosts()
+}
+
+var currentPostsInd = 0
+function nextPosts(){
     fetch("http://localhost:80/posts").then((response) => response.json())
         .then((data) => {
             posts = data.posts
-            console.log(posts)
-            let postsElements = new Array(posts.length)
-            for (let i = 0; i < posts.length; i++){
+            let len = Math.min(5, posts.length-currentPostsInd)
+            let postsElements = new Array(len)
+
+            for (let i = currentPostsInd; i < currentPostsInd+len; i++){
                 postsElements[i] = React.createElement(aPost, {key: posts[i].time+posts[i].body, posttime: posts[i].time, postbody:posts[i].body})
             }
             const rootElement =
@@ -24,6 +30,38 @@ function start() {
                     React.createElement("tbody", {}, postsElements)
                 );
             ReactDOM.render(rootElement, document.getElementById("mainContainer"));
+            currentPostsInd += len
+            console.log(currentPostsInd)
+            console.log(posts.length)
+            if (currentPostsInd == posts.length) {
+                let btt = document.getElementById("nextButton")
+                btt.style.display = 'none'
+            }
         })
 }
 
+function previousPosts(){
+    fetch("http://localhost:80/posts").then((response) => response.json())
+        .then((data) => {
+            posts = data.posts
+            let len = 5
+            let postsElements = new Array(len)
+
+            for (let i = currentPostsInd-len; i < currentPostsInd; i++){
+                postsElements[i] = React.createElement(aPost, {key: posts[i].time+posts[i].body, posttime: posts[i].time, postbody:posts[i].body})
+            }
+            const rootElement =
+                React.createElement("table",{},
+                    React.createElement("tbody", {}, postsElements)
+                );
+            ReactDOM.render(rootElement, document.getElementById("mainContainer"));
+            currentPostsInd -= len
+            console.log(currentPostsInd)
+            console.log(posts.length)
+            if (currentPostsInd <= len) {
+                let btt = document.getElementById("previousButton")
+                btt.style.display = 'none'
+            }
+        })
+}
+// TODO: refresh previousButton and nextButton
